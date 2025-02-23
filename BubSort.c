038913a7e1a148b69cd9444a0a29a46c8541fad8
Kaushik <132214248+CursedFyre@users.noreply.github.com>
2025-02-24 01:08:50 +0530

    Add files via upload

---
+++ BubSort.c
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no swap
+        if (swapped == false)
+            break;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+int main()
+{
+    int n;
+    printf("Enter array size:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter array elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
